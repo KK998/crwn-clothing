c5cf8060d17faa037131864d6e8ac46dee8585a9
KK998 <klemen.komel@gmail.com>
2019-07-07 16:00:49 +0200

    updated app with shop page, collection preview component and collection item component. Added styling for the shop page.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './Pages/homepage/homepage.component';
+import ShopPage from './Pages/shop/shop.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} /> 
-        <Route path="/shop/hats" component={HatsPage} /> 
+        <Route path="/shop" component={ShopPage} /> 
       </Switch>
     </div>
   );

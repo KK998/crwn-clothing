ee650486892a9fdadebedd0d48bc5b31403b26e1
KK998 <klemen.komel@gmail.com>
2019-07-13 13:48:49 +0200

    Added action creator to the dropdown for user easy of use

--- src/Components/cart-dropdown/cart-dropdown.component.jsx
+++ src/Components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,12 +4,13 @@ import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
 	return (
 		<div className="cart-dropdown">
 			<div className="cart-items">
@@ -21,7 +22,11 @@ const CartDropdown = ({ cartItems, history }) => {
 					<span className="empty-message">Your cart is empty</span>
 				)}
 			</div>
-			<CustomButton onClick={() => history.push('/checkout')}>
+			<CustomButton
+				onClick={() => {
+					history.push('/checkout');
+					dispatch(toggleCartHidden());
+				}}>
 				GO TO CHECKOUT
 			</CustomButton>
 		</div>

99efb2eb9d82398588f6633b68e254987f32e8bd
KK998 <klemen.komel@gmail.com>
2019-07-17 15:43:05 +0200

    Added single category page and modified selectors and shop data for faster access

--- src/Components/collections-overview/collections-overview.component.jsx
+++ src/Components/collections-overview/collections-overview.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import PreviewCollection from '../preview-collection/preview-collection.component';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => {
+const CollectionsOverview = ({collections}) => {
+	console.log(collections)
 	return (
 		<div className="collection-overview">
 			{collections.map(({ id, ...otherCollectionProps }) => (
@@ -18,7 +19,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-	collections: selectCollections,
+	collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
